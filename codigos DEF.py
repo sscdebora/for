f0d2a13e8661de84a2e29c2588d190c68a3549c2
Debora Cristina <deboracritinasantos5@gmail.com>
2020-11-30 20:38:41 -0300

    funcaoDEF

---
+++ codigos DEF.py
@@ -0,0 +1,20 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+def soma(a,b):
+print(a+b)
+
+
+def sub(a,b):
+print(a-b)
+
+def mult(m,d):
+print(a*d)
+
+
+def div(dividendo,divisor):
+if divisor==0:
+print("divisor não pode ser 0")
+else:
+print(dividendo/divisor)
+
